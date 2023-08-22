2ffd78260cc4f16baa99b509093f8ba7fe229091
Jafmatute <josueari20@gmail.com>
2023-08-22 10:11:12 -0600

    Exercise- Showing an Alert

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,20 @@
+import Alert from './components/Alert';
 import Buttom from './components/Buttom';
+import { useState } from 'react';
 
 const App = () => {
+  const [alertVisible, setalertVisibility] = useState(false);
+
   return (
     <div>
-      <Buttom onClick={() => console.log('Clicked')}>Guardar</Buttom>
+      {alertVisible && (
+        <Alert onClose={() => setalertVisibility(!alertVisible)}>
+          Bienvenido
+        </Alert>
+      )}
+      <Buttom onClick={() => setalertVisibility(!alertVisible)}>
+        Mostrar alerta
+      </Buttom>
     </div>
   );
 };
