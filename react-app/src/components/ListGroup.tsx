80d18534eb11d5a2b9f10c30ee4686d14c1f01ca
Jafmatute <josueari20@gmail.com>
2023-08-21 22:50:16 -0600

    Conditional Rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,9 +1,12 @@
 function ListGroup() {
-  const items = ['new york', 'Tokio', 'Paris', 'Lodom'];
+  let items = ['new york', 'Tokio', 'Paris', 'Lodom'];
+
+  const message = items.length === 0 && <p>Items not found</p>;
 
   return (
     <>
       <h1>List</h1>
+      {message}
       <ul className='list-group'>
         {items.map((item) => (
           <li key={item}>{item}</li>
