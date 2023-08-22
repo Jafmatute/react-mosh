f8ed892600c5ade9db7d73e13460ad8cff755985
Jafmatute <josueari20@gmail.com>
2023-08-22 11:13:31 -0600

    CSS Modules

--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './ListGroup.css';
+import styles from './ListGroup.module.css';
 
 interface Props {
   items: string[];
@@ -16,7 +16,7 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
     <>
       <h1>{heading}</h1>
       {message}
-      <ul className='list-group'>
+      <ul className={[styles.listGroup, styles.container].join(' ')}>
         {items.map((item, index) => (
           <li
             className={
