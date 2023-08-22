65a37f5afb67e655956c534f3ca041436f2d208a
Jafmatute <josueari20@gmail.com>
2023-08-21 21:01:48 -0600

    Creating a ListGroup Component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,11 @@
 import Message from "./Message"
+import ListGroup from "./components/ListGroup";
 
 const App = () => {
 
   return (
     <div>
-      <Message />
+      <ListGroup />
     </div>
   );
 }
