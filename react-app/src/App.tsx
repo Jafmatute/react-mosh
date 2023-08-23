fb24d95ec3ecd8ed718d755a88b2513ec44cf165
Jafmatute <josueari20@gmail.com>
2023-08-23 09:34:14 -0600

    Building a Form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,34 +1,11 @@
-import ExpandableText from './components/ExpandableText';
+import Form from "./components/Form.tsx";
 
 const App = () => {
-  return (
-    <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam porro
-        blanditiis quos, sequi rerum adipisci optio sit illo! Distinctio
-        eligendi explicabo maxime illum provident, asperiores beatae quas
-        voluptate dignissimos. Necessitatibus ratione cupiditate soluta! Quaerat
-        porro modi rerum dolorem aperiam architecto error tempore repellendus
-        blanditiis. Autem saepe tempore, doloribus, minus magni vitae, sint
-        aliquam eum magnam maiores est? In ipsum voluptas consequatur laudantium
-        consectetur qui, quis laborum labore minus velit dolorem assumenda ut
-        soluta at? Ipsa saepe, quibusdam iste dolor doloribus nesciunt explicabo
-        debitis vero. Repellendus dolor ipsa consequatur vitae perspiciatis iste
-        natus architecto fuga. Earum, reiciendis maxime facere cumque, dicta
-        recusandae asperiores atque molestias quisquam rerum tenetur eligendi
-        tempore. Reiciendis vero deserunt distinctio quam libero aut natus nulla
-        a mollitia soluta odit adipisci dignissimos sapiente, perspiciatis dicta
-        minus porro fuga! Eos nihil laborum sunt error deleniti veniam saepe
-        illum, perspiciatis modi architecto facilis maxime asperiores aliquid
-        fugiat aperiam fuga repellendus ratione dolor. Optio adipisci dolore
-        assumenda repellat maiores soluta ducimus hic sequi debitis quasi
-        reprehenderit id, repellendus porro. Saepe dolores nihil veniam tempore
-        libero, aliquam quo consequatur qui, nemo ut non et incidunt, distinctio
-        harum aut officiis laboriosam. Quos similique culpa quidem quisquam
-        pariatur quas impedit est id dignissimos iste.
-      </ExpandableText>
-    </div>
-  );
+    return (
+        <div>
+            <Form/>
+        </div>
+    );
 };
 
 export default App;
