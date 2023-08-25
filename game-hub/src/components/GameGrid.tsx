9a3d73bd56d6199b1ce2502c151334a75153022c
Jafmatute <josueari20@gmail.com>
2023-08-25 13:26:33 -0600

    Improving User Experience with Loading Skeletons

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -1,16 +1,18 @@
 import useGames from "../hooks/useGames.ts";
 import {SimpleGrid} from "@chakra-ui/react";
 import GameCard from "./GameCard.tsx";
+import GameCardSkeleton from "./GameCardSkeleton.tsx";
 
 const GameGrid = () => {
-   const {games, error} =useGames();
-
+    const {games, error, isLoading} = useGames();
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
         <>
-        {error && <p className="text-danger">{error}</p>}
-        <SimpleGrid columns={{sm:1,md:2, lg:3, xl:5}} padding="10px" spacing={10}>
-            {games.map(game=> <GameCard key={game.id} game={game} />)}
-        </SimpleGrid>
+            {error && <p className="text-danger">{error}</p>}
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding="10px" spacing={10}>
+                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+                {games.map(game => <GameCard key={game.id} game={game}/>)}
+            </SimpleGrid>
         </>
     )
 }
--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -25,16 +25,22 @@ const useGames = () => {
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
 
         const controller = new AbortController();
 
+        setLoading(true);
         apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
-            .then(rsp => setGames(rsp.data.results))
+            .then(rsp => {
+                setGames(rsp.data.results);
+                setLoading(false)
+            })
             .catch(err => {
                 if (err instanceof CanceledError) return;
-                setError(err.message)
+                setError(err.message);
+                setLoading(false);
             });
 
         return () => controller.abort();
@@ -43,6 +49,7 @@ const useGames = () => {
     return {
         games,
         error,
+        isLoading,
     }
 }
 
