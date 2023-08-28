9e0fa643deee4cfc70cad8036a6edf384bab5779
Jafmatute <josueari20@gmail.com>
2023-08-28 11:56:47 -0600

    Cleaning Up the Game Cards

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = ({gameQuery}: Props) => {
     return (
         <>
             {error && <p className="text-danger">{error}</p>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding="10px" spacing={3}>
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" spacing={6}>
                 {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}>
                     <GameCardSkeleton/>
                 </GameCardContainer>)}
