6fed39c7e213ca3ea86c5978f282931a280fb6dc
Jafmatute <josueari20@gmail.com>
2023-08-31 14:52:22 -0600

    Handling Mutation Errors

--- react-app/src/react-query/TodoForm.tsx
+++ react-app/src/react-query/TodoForm.tsx
@@ -7,7 +7,7 @@ const TodoForm = () => {
 
     const queryClient = useQueryClient();
 
-    const addTodo = useMutation({
+    const addTodo = useMutation<Todo, Error, Todo>({
         mutationFn:(todo:Todo) => axios.post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
             .then(rsp=> rsp.data),
         onSuccess: (saveTodo) => {
@@ -26,23 +26,26 @@ const TodoForm = () => {
     const ref = useRef<HTMLInputElement>(null);
 
     return (
-        <form className="row mb-3" onSubmit={(evt)=> {
-            evt.preventDefault();
-            if(ref.current && ref.current.value)
-            addTodo.mutate({
-                userId: 1,
-                id: 0,
-                title: ref.current?.value,
-                complete: false
-            })
-        }}>
-            <div className="col">
-                <input ref={ref} type='text' className="form-control" />
-            </div>
-            <div className="col">
-                <button className="btn btn-primary">Add</button>
-            </div>
-        </form>
+        <>
+            {addTodo.error &&  <div className="alert-danger">{addTodo.error.message}</div>}
+            <form className="row mb-3" onSubmit={(evt)=> {
+                evt.preventDefault();
+                if(ref.current && ref.current.value)
+                    addTodo.mutate({
+                        userId: 1,
+                        id: 0,
+                        title: ref.current?.value,
+                        complete: false
+                    })
+            }}>
+                <div className="col">
+                    <input ref={ref} type='text' className="form-control" />
+                </div>
+                <div className="col">
+                    <button className="btn btn-primary">Add</button>
+                </div>
+            </form>
+        </>
     )
 }
 export default TodoForm;
