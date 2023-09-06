ffb8cbd806ba6508e68572f2c87d1998e5e63bcd
Jafmatute <josueari20@gmail.com>
2023-09-06 15:46:04 -0600

    Getting Data about the Current Route

--- react-app/src/routing/UserDetailPage.tsx
+++ react-app/src/routing/UserDetailPage.tsx
@@ -1,6 +1,22 @@
+import {useParams} from "react-router-dom";
+
 const UserDetailPage = () => {
+    const params = useParams();
+    //search params
+    // const [searchParams, setSearchParams] = useSearchParams();
+    //console.log(searchParams.get("name"));
+
+    //location path
+    // const location = useLocation();
+    // console.log(location);
+
+
     return (
-        <div>UserDetailPage</div>
+        <>
+            <p>User ID:
+                <span className="badge text-bg-secondary">{params.id}</span>
+            </p>
+        </>
     )
 }
 export default UserDetailPage
