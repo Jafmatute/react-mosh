91535f10952f72bb61a533ed868c7d6331f06450
Jafmatute <josueari20@gmail.com>
2023-09-07 14:18:59 -0600

    Exercise- Fixing the NavBar

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import {HStack, Image} from "@chakra-ui/react";
 import logo from '../assets/game-hub.webp';
 import ColorModeSwitch from "./ColorModeSwitch.tsx";
 import SearchInput from "./SearchInput.tsx";
+import {Link} from "react-router-dom";
 
 const NavBar = () => {
     return (
         <HStack justifyContent="space-between" padding="10px">
-            <Image src={logo} boxSize="70px"/>
+            <Link to="/">
+                <Image src={logo} boxSize="70px" objectFit="cover"/>
+            </Link>
             <SearchInput/>
             <ColorModeSwitch/>
         </HStack>
