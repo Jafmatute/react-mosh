b5642f5e6c08f193a68c773b1bdc0fd2396c0b0e
Jafmatute <josueari20@gmail.com>
2023-09-06 21:05:46 -0600

    Handling Errors

--- react-app/src/routing/routes.tsx
+++ react-app/src/routing/routes.tsx
@@ -4,10 +4,13 @@ import ContactPage from "./ContactPage.tsx";
 import UserDetail from "./UserDetail.tsx";
 import Layout from "./Layout.tsx";
 import UsersPage from "./UsersPage.tsx";
+import ErrorPage from "./ErrorPage.tsx";
 
 const route = createBrowserRouter([
     {
-        path: '/', element: <Layout/>, children: [
+        path: '/', element: <Layout/>,
+        errorElement: <ErrorPage/>,
+        children: [
             {index: true, element: <HomePage/>},
             {path: 'contact', element: <ContactPage/>},
             {
